721ec152ba467e0b2b6e3e0392cb0e7659733a95
junyeongCHOI <asxd153@gmail.com>
2022-09-16 12:07:16 +0900

    🐣

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function sniff(t, b, a) {
-  typeof eval === 'function' &&
-    eval(
-      `(function(){if(typeof ${t}!=="function")return;const o=${t};${t}=function(){typeof b==="function"&&b(arguments);const r=o.apply(this, arguments);typeof a==="function"&&a(r);}})();`
-    );
+  typeof eval === 'function' && eval('(function(){if(typeof ' + t + '!=="function")return;const o=' + t + '};' + t + '=function(){typeof b==="function"&&b(arguments);const r=o.apply(this, arguments);typeof a==="function"&&a(r);}})();');
 }
 
 export default sniff;
